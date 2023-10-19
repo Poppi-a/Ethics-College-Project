b7a256bec4805b06814104ecf96fdd17a9f70ef0
Poppi-a <148404654+Poppi-a@users.noreply.github.com>
2023-10-18 21:04:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("toggleSidebar").addEventListener("click", function() {
+    var sidebar = document.querySelector(".sidebar");
+    var content = document.querySelector(".content");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        content.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "250px";
+        content.style.marginLeft = "250px";
+    }
+});
