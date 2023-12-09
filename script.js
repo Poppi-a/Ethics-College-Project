f8f130cdec0459f105d4c3577c741b19d3640c01
Poppi-a <148404654+Poppi-a@users.noreply.github.com>
2023-12-08 20:05:01 -0600

    final verson

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("toggleSidebar").addEventListener("click", function() {
     var sidebar = document.querySelector(".sidebar");
     var content = document.querySelector(".content");
     if (sidebar.style.width === "250px") {
-        sidebar.style.width = "50px";
+        sidebar.style.width = "100px";
         content.style.marginLeft = "0";
     } else {
         sidebar.style.width = "250px";
